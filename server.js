a2bbca51d94adcd0fdc7ffbb2b2e781d2a0d3675
Tiago Gomes <tidgomes@gmail.com>
2023-05-23 10:06:26 -0300

    handle eventual errors in the requests

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import userRouter from './routes/userRoutes.js'
 import connectToDB from './config/db.js'
+import errorHandler, { notFound } from './utils/errorHandling.js'
 
 dotenv.config()
 const PORT = process.env.PORT || 5000
@@ -11,6 +12,9 @@ connectToDB()
 const app = express()
 
 app.use('/api/users', userRouter)
+
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(PORT, () => {
   console.log(`Server up and running on port ${PORT}`)
