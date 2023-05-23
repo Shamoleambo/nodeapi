023c1e882f9b8280f5ae641fd82bad774be34718
Tiago Gomes <tidgomes@gmail.com>
2023-05-23 09:16:37 -0300

    config dotenv usage

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 import express from 'express'
+import dotenv from 'dotenv'
 import userRouter from './routes/userRoutes.js'
+
+dotenv.config()
+
+const PORT = process.env.PORT || 5000
 
 const app = express()
 
 app.use('/api/users', userRouter)
 
-app.listen(3000, () => {
-  console.log(`Server up and running`)
+app.listen(PORT, () => {
+  console.log(`Server up and running on port ${PORT}`)
 })
