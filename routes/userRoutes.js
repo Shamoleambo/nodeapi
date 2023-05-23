c4a7c0fa9da65bb818e3692927d03e6b23cf0760
Tiago Gomes <tidgomes@gmail.com>
2023-05-23 12:11:13 -0300

    protect /logout, and /profile routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,12 +6,16 @@ import {
   getUserProfile,
   updateUserProfile
 } from '../controller/userController.js'
+import protect from '../middlewares/protectRoute.js'
 
 const router = express.Router()
 
 router.post('/', registerUser)
 router.post('/login', authUser)
-router.post('/logout', logoutUser)
-router.route('/profile').get(getUserProfile).put(updateUserProfile)
+router.post('/logout', protect, logoutUser)
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile)
 
 export default router
