f239f234aa0d13b4a8a27bc7408631d1b3ba14ca
Tiago Gomes <tidgomes@gmail.com>
2023-05-23 08:57:19 -0300

    use user routes in server.js

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 import express from 'express'
+import userRouter from './routes/userRoutes.js'
 
 const app = express()
 
-app.use('/', (req, res, next) => {
-  res.send('<h1>Hello my man!</h1>')
-})
+app.use('/api/users', userRouter)
 
 app.listen(3000, () => {
   console.log(`Server up and running`)
