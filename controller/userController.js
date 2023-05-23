4d1a7f99ecc7f47cae2abaef3cc4849d2895183d
Tiago Gomes <tidgomes@gmail.com>
2023-05-23 10:21:16 -0300

    save a dummy user using the User model

--- controller/userController.js
+++ controller/userController.js
@@ -1,6 +1,18 @@
-export const registerUser = (req, res) => {
-  res.status(200).json({ message: 'Register User' })
-}
+import asyncHandler from 'express-async-handler'
+import User from '../model/User.js'
+
+export const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body
+
+  const user = new User()
+
+  user.name = name
+  user.email = email
+  user.password = password
+
+  await user.save()
+  res.status(200).json({ user })
+})
 
 export const authUser = (req, res) => {
   res.status(200).json({ message: 'Authenticate User' })
