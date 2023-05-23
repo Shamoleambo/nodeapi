d0b0a2809a5b166d2883c93a5e785ed66e822552
Tiago Gomes <tidgomes@gmail.com>
2023-05-23 10:20:47 -0300

    parse urlencoded and json requests

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const PORT = process.env.PORT || 5000
 connectToDB()
 
 const app = express()
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 app.use('/api/users', userRouter)
 
